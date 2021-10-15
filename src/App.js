23a999ef499b75762d8ef035361eb354ed928885
pawor <pawor.emi@gmail.com>
2021-10-15 20:39:12 +0700

    edit pages

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react'
+//import { Link } from 'react-router-dom';
+//import { Layout } from 'antd';
+import { Navbar } from './components';
+import './App.css';
+
+const App = () => {
+    return (
+        <div className="app">
+            <div className="navbar">
+                <Navbar />
+            </div>
+            <div className="main"></div>
+            <div className="footer"></div>
+        </div>
+    );
+}
+
+export default App
